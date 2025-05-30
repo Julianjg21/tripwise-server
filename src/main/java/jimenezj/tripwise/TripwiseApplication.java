41c828e9a6a9a6f90a09586e100d97c1cc739592
Julian Jimenez <julianjimenez2128@gmail.com>
2025-05-30 00:04:11 -0500

    Upload project structure

---
+++ src/main/java/jimenezj/tripwise/TripwiseApplication.java
@@ -0,0 +1,13 @@
+package jimenezj.tripwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripwiseApplication.class, args);
+	}
+
+}
