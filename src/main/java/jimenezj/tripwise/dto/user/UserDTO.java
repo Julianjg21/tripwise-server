2d5cf785abb8f661b665c1dc15345f65f072a1cc
Julian Jimenez <julianjimenez2128@gmail.com>
2025-05-30 19:56:34 -0500

    feat: add User and UserDTO classes for user management

---
+++ src/main/java/jimenezj/tripwise/dto/user/UserDTO.java
@@ -0,0 +1,8 @@
+package jimenezj.tripwise.dto.user;
+
+public record UserDTO(
+        int id,
+        String email,
+        String fullName
+) {
+}
---
+++ src/main/java/jimenezj/tripwise/model/User.java
@@ -0,0 +1,48 @@
+package jimenezj.tripwise.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
