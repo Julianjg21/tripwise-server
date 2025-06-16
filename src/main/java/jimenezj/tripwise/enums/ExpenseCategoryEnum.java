c1a1ec3d33b61c74c70e3cf5764d0a219e14fbb4
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-16 16:02:22 -0500

    refactor: rename ExpenseCategory to ExpenseCategoryEnum for more clarity

---
+++ src/main/java/jimenezj/tripwise/enums/ExpenseCategoryEnum.java
@@ -0,0 +1,13 @@
+package jimenezj.tripwise.enums;
+
+public enum ExpenseCategoryEnum {
+    TRANSPORT,
+    ACCOMMODATION,
+    FOOD,
+    ENTERTAINMENT,
+    SHOPPING,
+    ACTIVITIES,
+    INSURANCE,
+    TIPS,
+    OTHER
+}
