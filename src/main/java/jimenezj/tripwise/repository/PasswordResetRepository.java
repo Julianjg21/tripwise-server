dee7e71a8cce84820b01955e9cc24b54d5bcca94
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-20 20:42:54 -0500

    feat: add PasswordResetRepository for managing password reset tokens

---
+++ src/main/java/jimenezj/tripwise/repository/PasswordResetRepository.java
@@ -0,0 +1,12 @@
+package jimenezj.tripwise.repository;
+
+import jimenezj.tripwise.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
