af9f4167858bf8602a4ee9fcbfb1bb950bbbacae
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-20 20:42:29 -0500

    feat: add validation error handling in GlobalExceptionHandler

--- src/main/java/jimenezj/tripwise/exception/GlobalExceptionHandler.java
+++ src/main/java/jimenezj/tripwise/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package jimenezj.tripwise.exception;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jimenezj.tripwise.dto.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.time.LocalDateTime;
 
+
+// Custom exceptions
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private ErrorResponse buildError(HttpStatus status, String message, String path) {
@@ -16,34 +19,54 @@ public class GlobalExceptionHandler {
                 status.value(),
                 message,
                 path,
-                LocalDateTime.now()
-        );
+                LocalDateTime.now());
     }
 
-
+    // Exception handlers for generaly exceptions
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException ex, HttpServletRequest req) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(buildError(HttpStatus.BAD_REQUEST, ex.getMessage(), req.getRequestURI()));
     }
 
+    // Handle validation errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex,
+            HttpServletRequest req) {
+        String firstErrorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .orElse("Invalid request");
+
+        ErrorResponse error = buildError(HttpStatus.BAD_REQUEST, firstErrorMessage, req.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+
+    // Handle resource not found exceptions
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, HttpServletRequest req) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(buildError(HttpStatus.NOT_FOUND, ex.getMessage(), req.getRequestURI()));
     }
+
+
+    // Handle access denied exceptions
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex, HttpServletRequest req) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(buildError(HttpStatus.UNAUTHORIZED, ex.getMessage(), "Access denied"));
     }
 
+    // Handle all other exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAll(Exception ex, HttpServletRequest req) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(buildError(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred", req.getRequestURI()));
     }
 
+
+    // Handle unauthorized exceptions
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException ex, HttpServletRequest req) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
