297ceb7f00325305417d2d33e2ac9f36d7462af9
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-20 20:43:08 -0500

    feat: remove unused imports in TripRepository

--- src/main/java/jimenezj/tripwise/repository/TripRepository.java
+++ src/main/java/jimenezj/tripwise/repository/TripRepository.java
@@ -1,12 +1,10 @@
 package jimenezj.tripwise.repository;
 
-import jimenezj.tripwise.dto.trip.TripResponse;
 import jimenezj.tripwise.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
