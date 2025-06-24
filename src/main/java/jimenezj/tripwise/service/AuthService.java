4dd16bd62af781897753e0a2cba0ad768204d890
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-24 17:10:50 -0500

    feat: update AuthService interface to use AuthTokens for login and refreshToken methods

--- src/main/java/jimenezj/tripwise/service/AuthService.java
+++ src/main/java/jimenezj/tripwise/service/AuthService.java
@@ -1,17 +1,18 @@
 package jimenezj.tripwise.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jimenezj.tripwise.dto.auth.*;
 import jimenezj.tripwise.exception.BadRequestException;
 
 // Interface that defines the main auth methods
 public interface AuthService {
     // User login, returns access + refresh tokens
-    AuthResponse login(LoginRequest request);
+    AuthTokens login(LoginRequest request);
 
     // Register new user
     void signup(SignupRequest request) throws BadRequestException;
 
     // Generate new access token using refresh token
-    AuthRefreshResponse refreshToken(RefreshTokenRequest request);
+    AuthTokens refreshToken(HttpServletRequest request);
 
 }
